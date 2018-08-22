8378e9a1e87b7cf2eab7dac6851fb13b8a8a172e
Henesis Espinosa <henz001.espinosa@gmail.com>
2018-08-22 19:17:01 +0800

    My Relative Layout Laboratory Activity

---
+++ app/src/main/java/com/espinosa/relativelayout/RealativeLayout.java
@@ -0,0 +1,13 @@
+package com.espinosa.relativelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RealativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_realative_layout);
+    }
+}
